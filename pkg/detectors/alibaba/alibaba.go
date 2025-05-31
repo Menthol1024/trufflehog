4af9b43265272c1a256c13a3655d59c1c5226e64
firebasky <2317103712@qq.com>
2025-05-31 19:38:26 +0800

    1. 修改ak 正则表达式 2. build

--- pkg/detectors/alibaba/alibaba.go
+++ pkg/detectors/alibaba/alibaba.go
@@ -44,7 +44,8 @@ var (
 
 	// Make sure that your group is surrounded in boundary characters such as below to reduce false positives.
 	keyPat = regexp.MustCompile(`\b([a-zA-Z0-9]{30})\b`)
-	idPat  = regexp.MustCompile(`\b(LTAI[a-zA-Z0-9]{17,21})[\"';\s]*`)
+	// 匹配 LTAI 开头，后面跟着 12、16、17、18、20、21、22 位的 alnum 字符串
+	idPat = regexp.MustCompile(`\b(LTAI[a-zA-Z0-9]{12,22})[\"';\s]*`)
 )
 
 // Keywords are used for efficiently pre-filtering chunks.
